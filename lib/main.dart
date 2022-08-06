7478952d97c87bf0896ce98bd8dc8b62444bc345
LordOlumide <olumideolubosede@yahoo.com>
2022-08-06 21:02:35 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'pages/home_page.dart';
+
+import 'package:google_fonts/google_fonts.dart';
+import 'package:charts_flutter/flutter.dart';
+import 'package:data_table_2/data_table_2.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ,
+    );
+  }
+}
